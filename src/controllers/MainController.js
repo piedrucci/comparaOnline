5415d31f14dcc239715a1af6a7fba6943138728a
Roberth Mejias <piedrucci@gmail.com>
2018-07-29 21:58:23 -0400

    generate txt report

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -8,14 +8,14 @@ const Coverage = globals.productTypes;
 const proccessProducts = (req, res) => {
     const productsAtDayZero = [
         new Product(Coverage.medium, 10, 20),
-        new Product(Coverage.full, 6, 6),
+        new Product(Coverage.full, 2, 0),
         new Product(Coverage.low, 5, 7),
         new Product(Coverage.mega, 0, 80),
-        new Product(Coverage.mega, 4, 80),
+        new Product(Coverage.mega, -1, 80),
         new Product(Coverage.special, 15, 20),
         new Product(Coverage.special, 10, 49),
-        new Product(Coverage.special, 5, 35),
-        new Product(Coverage.superSale, 3, 7),
+        new Product(Coverage.special, 5, 49),
+        new Product(Coverage.superSale, 3, 6),
     ];
     const carInsurance = new CarInsurance(productsAtDayZero);
 
